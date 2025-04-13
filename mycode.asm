f4c2b1dd2efb493a6a7ba632c372f8ff8c4d90ff
phamduccua <phamcua84@gmail.com>
2025-04-13 09:39:07 +0700

    a

--- mycode.asm
+++ mycode.asm
@@ -72,280 +72,7 @@ start:
     mov cl, 0       
     mov dh, 24    
     mov dl, 79
-    int 10h 
-
-    mov ah,4ch
-    int 21h
-    
-    ret
-instruction1 proc 
-    mov ah, 6       
-    mov al, 0       
-    mov bh, 07h    
-    mov ch, 0       
-    mov cl, 0       
-    mov dh, 24    
-    mov dl, 79
-    int 10h 
-    
-    call border1
-    
-    lea si, instructioN
-    mov di, 160 * 3 + 30 * 2
-    call print_string 
-    
-    lea si, uP1
-    mov di, 160 * 5 + 30 * 2
-    call print_string
-    
-    mov al, 24
-    mov ah, 0Fh      
-    mov word ptr es:[di], ax
-                
-    lea si, dowN1
-    mov di, 160 * 7 + 30 * 2
-    call print_string 
-    
-    mov al, 25
-    mov ah, 0Fh      
-    mov word ptr es:[di], ax
-
-    lea si, lefT1
-    mov di, 160 * 9 + 30 * 2
-    call print_string
-    
-    mov al, 27
-    mov ah, 0Fh      
-    mov word ptr es:[di], ax
-                  
-    lea si, righT1
-    mov di, 160 * 11 + 30 * 2
-    call print_string
-    
-    mov al, 26
-    mov ah, 0Fh      
-    mov word ptr es:[di], ax
-    
-    
-
-    ret
-    
-instruction1 endp
-;che do dia chi segment moi ki tu chu 2 byte 1 byte luu ma assci 1 byte luu ma mau                                   
-;moi dong co 80 cot *2 byte = 160 byte 
-;moi ky tu chua 2 byte den di den dung vi tri cot thi ta can nhay den cot*2
-;cach tinh vi tri (row*160)+(col*2)
-main_menu proc
-    mov ah, 6       
-    mov al, 0       
-    mov bh, 07h    
-    mov ch, 0       
-    mov cl, 0       
-    mov dh, 24    
-    mov dl, 79
-    int 10h 
-    lea si, header
-    mov di, 160 * 5 + 30 * 2
-    call print_string
-
-   
-    lea si, main1
-    mov di, 160 * 7 + 25 * 2
-    call print_string
-
-    lea si, main2
-    mov di, 160 * 9 + 10 * 2
-    call print_string
-
-    lea si, main3
-    mov di, 160 * 11 + 28 * 2
-    call print_string
-
-    ret
-main_menu endp
-;lodsb :
-;nap chuoi nguon byte vao thanh ghi al
-;al=[si],si++(df=0)
-print_string proc
-next_char:
-    lodsb            
-    cmp al, 0
-    je done
-    ;es tro vao vung nho
-    ;suy ra trong es co 2 bit
-    mov es:[di], al  ;ghi ki tu vao man hinh 
-    mov es:[di+1], 0Ah;hien thi mau sac cua ki tu
-    add di, 2
-    jmp next_char
-done:
-    ret
-print_string endp
-gmpl1 proc
-    mov ah, 6       
-    mov al, 0       
-    mov bh, 07h    
-    mov ch, 0       
-    mov cl, 0       
-    mov dh, 24    
-    mov dl, 79
-    int 10h 
-    
-    call border1
-    lea si, seclect1
-    mov di, 160 * 3 + 30 * 2
-    call print_string 
-    
-    lea si, m1
-    mov di, 160 * 5 + 30 * 2
-    call print_string
-
-    lea si, m2
-    mov di, 160 * 7 + 30 * 2
-    call print_string 
-
-    lea si, m3
-    mov di, 160 * 9 + 30 * 2
-    call print_string
-                       
-    lea si, m4
-    mov di, 160 * 11 + 30 * 2
-    call print_string 
-    
-    mov ah, 0
-    int 16h
-     cmp al, '1'
-    je select_map1
-    cmp al, '2'
-    je select_map2
-    cmp al, '3'
-    je select_map3
-    cmp al, '4'
-    je select_map4
-    jmp chosegameplay_end  
-    select_map1:
-        mov seclectmapindex, 1
-        jmp chosegameplay_end
-    
-    select_map2:
-        mov seclectmapindex, 2
-        jmp chosegameplay_end
-    
-    select_map3:
-        mov seclectmapindex, 3
-        jmp chosegameplay_end
-    
-    select_map4:
-        mov seclectmapindex, 4
-        jmp chosegameplay_end 
-endp
-gmpl2 proc
-    mov ah, 6       
-    mov al, 0       
-    mov bh, 07h    
-    mov ch, 0       
-    mov cl, 0       
-    mov dh, 24    
-    mov dl, 79
-    int 10h 
-    
-    call border1
-    lea si, seclect2
-    mov di, 160 * 3 + 30 * 2
-    call print_string 
-    
-    lea si, m1
-    mov di, 160 * 5 + 30 * 2
-    call print_string
-
-    lea si, m2
-    mov di, 160 * 7 + 30 * 2
-    call print_string 
-
-    lea si, m3
-    mov di, 160 * 9 + 30 * 2
-    call print_string
-                       
-    lea si, m4
-    mov di, 160 * 11 + 30 * 2
-    call print_string 
-    
-    mov ah, 0
-    int 16h
-    
-    jmp chosegameplay_end
-    cmp al, '1'
-    je 1select_map1
-    cmp al, '2'
-    je 1select_map2
-    cmp al, '3'
-    je 1select_map3
-    cmp al, '4'
-    je 1select_map4
-    jmp chosegameplay_end  
-    1select_map1:
-        mov seclectmapindex, 1
-        jmp chosegameplay_end
-    
-    1select_map2:
-        mov seclectmapindex, 2
-        jmp chosegameplay_end
-    
-    1select_map3:
-        mov seclectmapindex, 3
-        jmp chosegameplay_end
-    
-    1select_map4:
-        mov seclectmapindex, 4
-        jmp chosegameplay_end  
-endp
-chosegameplay proc
-    mov ah, 6       
-    mov al, 0       
-    mov bh, 07h    
-    mov ch, 0       
-    mov cl, 0       
-    mov dh, 24    
-    mov dl, 79
-    call border1    
-
-    lea si, seclect
-    mov di, 160 * 3 + 30 * 2
-    call print_string 
-
-    lea si, gameplay1
-    mov di, 160 * 7 + 30 * 2
-    call print_string 
-
-    lea si, gameplay2
-    mov di, 160 * 9 + 30 * 2
-    call print_string
-    
-    mov ah, 0
-    int 16h
-
-    cmp al, '1'
-    je call_gmpl1
-    
-    cmp al, '2'
-    je call_gmpl2
-    
-    call_gmpl1:
-        call gmpl1
-        jmp chosegameplay_end
-    
-    call_gmpl2:
-        call gmpl2
-        jmp chosegameplay_end
- 
-    
-     
-    chosegameplay_end:
-    ret
-    
- 
-    
-
-chosegameplay endp         
+    int 10h          
 begin: 
     mov direction, 1
     call border3
@@ -662,5 +389,276 @@ begin:
         call clearSnack
         call begin
         ret 
-ret                 
+ret
+
+
+    
+instruction1 proc 
+    mov ah, 6       
+    mov al, 0       
+    mov bh, 07h    
+    mov ch, 0       
+    mov cl, 0       
+    mov dh, 24    
+    mov dl, 79
+    int 10h 
+    
+    call border1
+    
+    lea si, instructioN
+    mov di, 160 * 3 + 30 * 2
+    call print_string 
+    
+    lea si, uP1
+    mov di, 160 * 5 + 30 * 2
+    call print_string
+    
+    mov al, 24
+    mov ah, 0Fh      
+    mov word ptr es:[di], ax
+                
+    lea si, dowN1
+    mov di, 160 * 7 + 30 * 2
+    call print_string 
+    
+    mov al, 25
+    mov ah, 0Fh      
+    mov word ptr es:[di], ax
+
+    lea si, lefT1
+    mov di, 160 * 9 + 30 * 2
+    call print_string
+    
+    mov al, 27
+    mov ah, 0Fh      
+    mov word ptr es:[di], ax
+                  
+    lea si, righT1
+    mov di, 160 * 11 + 30 * 2
+    call print_string
+    
+    mov al, 26
+    mov ah, 0Fh      
+    mov word ptr es:[di], ax
+    
+    
+
+    ret
+    
+instruction1 endp
+;che do dia chi segment moi ki tu chu 2 byte 1 byte luu ma assci 1 byte luu ma mau                                   
+;moi dong co 80 cot *2 byte = 160 byte 
+;moi ky tu chua 2 byte den di den dung vi tri cot thi ta can nhay den cot*2
+;cach tinh vi tri (row*160)+(col*2)
+main_menu proc
+    mov ah, 6       
+    mov al, 0       
+    mov bh, 07h    
+    mov ch, 0       
+    mov cl, 0       
+    mov dh, 24    
+    mov dl, 79
+    int 10h 
+    lea si, header
+    mov di, 160 * 5 + 30 * 2
+    call print_string
+
+   
+    lea si, main1
+    mov di, 160 * 7 + 25 * 2
+    call print_string
+
+    lea si, main2
+    mov di, 160 * 9 + 10 * 2
+    call print_string
+
+    lea si, main3
+    mov di, 160 * 11 + 28 * 2
+    call print_string
+
+    ret
+main_menu endp
+;lodsb :
+;nap chuoi nguon byte vao thanh ghi al
+;al=[si],si++(df=0)
+print_string proc
+next_char:
+    lodsb            
+    cmp al, 0
+    je done
+    ;es tro vao vung nho
+    ;suy ra trong es co 2 bit
+    mov es:[di], al  ;ghi ki tu vao man hinh 
+    mov es:[di+1], 0Ah;hien thi mau sac cua ki tu
+    add di, 2
+    jmp next_char
+done:
+    ret
+print_string endp
+gmpl1 proc
+    mov ah, 6       
+    mov al, 0       
+    mov bh, 07h    
+    mov ch, 0       
+    mov cl, 0       
+    mov dh, 24    
+    mov dl, 79
+    int 10h 
+    
+    call border1
+    lea si, seclect1
+    mov di, 160 * 3 + 30 * 2
+    call print_string 
+    
+    lea si, m1
+    mov di, 160 * 5 + 30 * 2
+    call print_string
+
+    lea si, m2
+    mov di, 160 * 7 + 30 * 2
+    call print_string 
+
+    lea si, m3
+    mov di, 160 * 9 + 30 * 2
+    call print_string
+                       
+    lea si, m4
+    mov di, 160 * 11 + 30 * 2
+    call print_string 
+    
+    mov ah, 0
+    int 16h
+     cmp al, '1'
+    je select_map1
+    cmp al, '2'
+    je select_map2
+    cmp al, '3'
+    je select_map3
+    cmp al, '4'
+    je select_map4
+    jmp chosegameplay_end  
+    select_map1:
+        mov seclectmapindex, 1
+        jmp chosegameplay_end
+    
+    select_map2:
+        mov seclectmapindex, 2
+        jmp chosegameplay_end
+    
+    select_map3:
+        mov seclectmapindex, 3
+        jmp chosegameplay_end
+    
+    select_map4:
+        mov seclectmapindex, 4
+        jmp chosegameplay_end 
+endp
+gmpl2 proc
+    mov ah, 6       
+    mov al, 0       
+    mov bh, 07h    
+    mov ch, 0       
+    mov cl, 0       
+    mov dh, 24    
+    mov dl, 79
+    int 10h 
+    
+    call border1
+    lea si, seclect2
+    mov di, 160 * 3 + 30 * 2
+    call print_string 
+    
+    lea si, m1
+    mov di, 160 * 5 + 30 * 2
+    call print_string
+
+    lea si, m2
+    mov di, 160 * 7 + 30 * 2
+    call print_string 
+
+    lea si, m3
+    mov di, 160 * 9 + 30 * 2
+    call print_string
+                       
+    lea si, m4
+    mov di, 160 * 11 + 30 * 2
+    call print_string 
+    
+    mov ah, 0
+    int 16h
+    
+    jmp chosegameplay_end
+    cmp al, '1'
+    je 1select_map1
+    cmp al, '2'
+    je 1select_map2
+    cmp al, '3'
+    je 1select_map3
+    cmp al, '4'
+    je 1select_map4
+    jmp chosegameplay_end  
+    1select_map1:
+        mov seclectmapindex, 1
+        jmp chosegameplay_end
+    
+    1select_map2:
+        mov seclectmapindex, 2
+        jmp chosegameplay_end
+    
+    1select_map3:
+        mov seclectmapindex, 3
+        jmp chosegameplay_end
+    
+    1select_map4:
+        mov seclectmapindex, 4
+        jmp chosegameplay_end  
+endp
+chosegameplay proc
+    mov ah, 6       
+    mov al, 0       
+    mov bh, 07h    
+    mov ch, 0       
+    mov cl, 0       
+    mov dh, 24    
+    mov dl, 79
+    call border1    
+
+    lea si, seclect
+    mov di, 160 * 3 + 30 * 2
+    call print_string 
+
+    lea si, gameplay1
+    mov di, 160 * 7 + 30 * 2
+    call print_string 
+
+    lea si, gameplay2
+    mov di, 160 * 9 + 30 * 2
+    call print_string
+    
+    mov ah, 0
+    int 16h
+
+    cmp al, '1'
+    je call_gmpl1
+    
+    cmp al, '2'
+    je call_gmpl2
+    
+    call_gmpl1:
+        call gmpl1
+        jmp chosegameplay_end
+    
+    call_gmpl2:
+        call gmpl2
+        jmp chosegameplay_end
+ 
+    
+     
+    chosegameplay_end:
+    ret
+    
+ 
+    
+
+chosegameplay endp                 
 end start
